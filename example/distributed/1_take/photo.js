40bc1ce7dd2f121b97a45517a0c21cbdf50fc404
alexander34ro <ardeleanalexandru3@gmail.com>
2021-03-09 21:20:11 +0200

    Add support for multiple photo and video streams

---
+++ example/distributed/1_take/photo.js
@@ -0,0 +1,18 @@
+const NodeWebcam = require('node-webcam');
+const exec = require('child_process').exec;
+const os = require('os');
+
+if (require.main === module) {
+  const path = '1_camera_sample_' + Date.now() + '.jpg';
+  if (os.platform() === 'android') {
+    exec(
+      `termux-camera-photo -c 1 ${path}`,
+      (error, stdout, stderr) => {
+        console.log(path);
+      }
+    );
+  } else {
+    const Webcam = NodeWebcam.create();
+    Webcam.capture(path, function (err, data) { console.log(data); });
+  }
+}
---
+++ example/distributed/2_take/video.js
@@ -0,0 +1,36 @@
+const exec = require('child_process').exec;
+const os = require('os');
+
+if (require.main === module) {
+  const path = 'video_sample_' + Date.now() + '.mp4';
+  const input = '-i default';
+  const framerate = '-framerate 30';
+  const time = '-t 4';
+
+  let format;
+
+  if (os.platform() === 'android') {
+    exec(
+      `termux-camera-photo -c 1 ${path}`,
+      () => {
+        console.log(path);
+      }
+    );
+  } else if (os.platform() === 'darwin') {
+    format = '-f avfoundation';
+    exec(
+      `ffmpeg ${format} ${framerate} ${input} ${time} ${path}`,
+      () => {
+        console.log(path);
+      }
+    );
+  } else {
+    format = '-f v4l2';
+    exec(
+      `ffmpeg ${format} ${framerate} ${input} ${time} ${path}`,
+      () => {
+        console.log(path);
+      }
+    );
+  }
+}
