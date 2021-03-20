abe52b89edd46abe9ba8007ad3118cfef17f0662
alexander34ro <ardeleanalexandru3@gmail.com>
2021-03-20 21:57:29 +0200

    Cleanup

--- example/client/2_take/video.js
+++ example/client/2_take/video.js
@@ -8,10 +8,12 @@ if (require.main === module) {
 
   let format;
   let input;
+  let command;
 
   if (os.platform() === 'android') {
+    command = `termux-camera-photo -c 1 ${path}`;
     exec(
-      `termux-camera-photo -c 1 ${path}`,
+      command,
       () => {
         console.log(path);
       }
@@ -19,17 +21,29 @@ if (require.main === module) {
   } else if (os.platform() === 'darwin') {
     format = '-f avfoundation';
     input = '-i default';
+    command = `ffmpeg ${format} ${framerate} ${input} ${time} ${path}`;
     exec(
-      `ffmpeg ${format} ${framerate} ${input} ${time} ${path}`,
+      command,
+      () => {
+        console.log(path);
+      }
+    );
+  } else if (os.platform() === 'linux') {
+    format = '-f v4l2';
+    input = '-i /dev/video0';
+    command = `ffmpeg ${format} ${framerate} ${input} ${time} ${path}`;
+    exec(
+      command,
       () => {
         console.log(path);
       }
     );
   } else {
     format = '-f v4l2';
-    input = '-i /dev/video0'
+    input = '-i /dev/video0';
+    command = `ffmpeg ${format} ${framerate} ${input} ${time} ${path}`;
     exec(
-      `ffmpeg ${format} ${framerate} ${input} ${time} ${path}`,
+      command,
       () => {
         console.log(path);
       }
