2e17a34d8e66a78a2d3df78f074a37104b5484f4
alexander34ro <ardeleanalexandru3@gmail.com>
2021-03-24 21:23:15 +0200

    Support for tensorflow lite

--- example/server/2_emotion_detector/emotions.py
+++ example/server/2_emotion_detector/emotions.py
@@ -1,40 +1,17 @@
 import numpy as np
 import cv2
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPooling2D
-# import tensorflow as tf
+from tensorflow import lite
 import sys
 import json
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-def create_model():
-    model = Sequential()
-
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(7, activation='softmax'))
-
-    model.load_weights('model.h5')
-
-    return model
-
 def emotion(file):
-    model = create_model()
+    # load lite model
+    interpreter = lite.Interpreter(model_path="model.tflite")
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
     # prevents openCL usage and unnecessary logging messages
     cv2.ocl.setUseOpenCL(False)
     # dictionary which assigns each label an emotion (alphabetical order)
@@ -56,10 +33,11 @@ def emotion(file):
             cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
             roi_gray = gray[y:y + h, x:x + w]
             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
 
-            # interpreter = tf.lite.Interpreter(model_path="model.tflite")
-            # interpreter.allocate_tensors()
+            input_data = np.array(cropped_img, dtype=np.float32)
+            interpreter.set_tensor(input_details[0]['index'], input_data)
+            interpreter.invoke()
+            prediction = interpreter.get_tensor(output_details[0]['index'])
 
             maxindex = int(np.argmax(prediction))
             cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
